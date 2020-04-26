9cee0c15313082cafd0f07130dc797fed474b480
CragonLong <155190625@qq.com>
2020-04-26 09:32:17 +0800

    Spring Cloud整合nacos，对nacos进行简单配置

--- spring-cloud-nacos-config/src/main/java/com/cragon/controller/TestController.java
+++ spring-cloud-nacos-config/src/main/java/com/cragon/controller/TestController.java
@@ -12,9 +12,18 @@ import javax.validation.Valid;
 public class TestController {
     @Value("${message:null}")
     private String message;
+    @Value("${ext1:null}")
+    private String ext1;
+    @Value("${ext2:null}")
+    private String ext2;
 
     @GetMapping("message")
     public String getMessage() {
         return this.message;
+    }
+
+    @GetMapping("multi")
+    public String getMulti() {
+        return String.format("ext1: %s ext2: %s", ext1, ext2);
     }
 }
